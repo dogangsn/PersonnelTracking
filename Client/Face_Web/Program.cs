587176d779244fde2bec46ea5d9da0df234fe958
Dogan Günes <dogangns.98@gmail.com>
2022-06-20 23:58:17 +0300

    .

--- Client/Face_Web/Program.cs
+++ Client/Face_Web/Program.cs
@@ -16,6 +16,7 @@ namespace Face_Web
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
